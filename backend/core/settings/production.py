d4b56a545d71fd449005ad14323f3f128826e966
AmirBigdeli <141164942+varlamzhordania@users.noreply.github.com>
2023-11-22 18:50:23 +0400

    Update production.py

--- backend/core/settings/production.py
+++ backend/core/settings/production.py
@@ -9,15 +9,24 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("DB_NAME", "whatsappPartnersDB"),
+        'USER': os.getenv("DB_USER", "whatsappPartnersAdmin"),
+        'PASSWORD': os.getenv("DB_PASSWORD", "aD5%w12Gx_&8"),
+        'HOST': "postgres-partners",  # Use the service name from docker-compose.yml
+        'PORT': '5432',
+    }
+}
+
+CACHES = {
+    'default': {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        'LOCATION': os.getenv("REDIS_HOST"),  # Use the REDIS_HOST environment variable
     }
 }
 
